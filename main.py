4f5ecda7295ea6db039a31a11ff1898c337c92b5
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2024-06-19 15:10:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,48 +4,24 @@ import asyncio
 import os
 import random
 import always_on
-
+import requests
 class Client(revolt.Client):
   
   async def on_ready(self):
     print('Run  ルーレットBOT')
     
   async def on_message(self, message: revolt.Message):
-    guilds = self.servers
-      
-    
-    if message.content == '.r':
-      numbers = [random.randint(0, 9) for _ in range(3)]
-      await self.edit_status(text=f".r | Used in {len(guilds)} servers")
-      # 生成された乱数が全て同じかどうかチェック
-      if numbers[0] == numbers[1] == numbers[2]:
-          # Revoltのチャンネルに"good"と送信
-          await message.channel.send(f"good! {numbers}")
-          print(f"{numbers} - YES")
-      else:
-          # 乱数が異なる場合は、その乱数を送信
-          await message.channel.send(f"Generated numbers: {numbers}")
-          print(f"{numbers} - NO")
-
-      
-    if message.content == '.g':
-      numbers = [random.randint(0, 9) for _ in range(10)]
-
-      # 生成された乱数が全て同じかどうかチェック
-      if numbers[0] == numbers[1] == numbers[2] == numbers[3] == numbers[4] == numbers[5] == numbers[6] == numbers[7] == numbers[8] == numbers[9]:
-          # Revoltのチャンネルに"good"と送信
-          await message.channel.send(f"good! {numbers}")
-          print(f"{numbers} - YES")
-      else:
-          # 乱数が異なる場合は、その乱数を送信
-          await message.channel.send(f"Generated numbers: {numbers}")
-          print(f"{numbers} - NO")
-      
-      
-    
-
-
-
+    def receive_message(message):
+    # メッセージが "yd " で始まるかをチェック
+     if message.startswith("yd "):
+        # "yd "を取り除いた後の部分が文字列として取得されます
+        video_id = message[3:]
+        # ここでqueryを使って適切な処理を行います
+        api_url = f'https://invidious.example/api/v1/videos/{video_id}'
+        response = requests.get(api_url)
+        data = response.json()
+        video_url = data['formats'][0]['url']
+        await message.channel.send(f"Video URL:{video_url}")
 async def main():
   async with revolt.utils.client_session() as session:
     client = Client(session, os.environ['R'])
